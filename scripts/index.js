650de653b87921af6b8afbb0e7f6668b43ea903e
Mel <garrillofeeling@gmail.com>
2024-12-27 18:24:13 -0400

    Add mouse hover event
    Make the cell increase opacity when the mouse hovers over it.

--- scripts/index.js
+++ scripts/index.js
@@ -1,10 +1,18 @@
 let size = 16;
 
+function hoverCell(event) {
+    let opacityValue = parseFloat(event.target.style.opacity);
+    event.target.style.opacity = ((opacityValue + 0.1 <= 1) ? opacityValue + 0.1 : 1).toString();
+}
+
 function createCell() {
     let newCell = document.createElement("div");
     newCell.classList.add("cell");
     newCell.style.width = `${960 / size}px`;
     newCell.style.height = `${960 / size}px`;
+    newCell.style.opacity = 0;
+    newCell.style.backgroundColor = "black";
+    newCell.addEventListener("mouseover", hoverCell);
     return newCell;
 }
 
